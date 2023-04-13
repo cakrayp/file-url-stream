839052c09978f65faa6f276022c42d5d9619e7ef
cakrayp <cakrayp@gmail.com>
2023-04-13 16:50:19 +0700

    Try to access cloudflare bypass security and fix

--- router/files.js
+++ router/files.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const router = express.Router();
 const { default: Axios } = require("axios");
-const Humanoid = require("humanoid-js");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 // const fs = require("fs");
